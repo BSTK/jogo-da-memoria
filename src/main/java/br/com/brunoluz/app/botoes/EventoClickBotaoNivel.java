f19f0cb3877c381731d712a173b6092936d72a05
BSTK <bsilva.se@gmail.com>
2017-01-06 12:17:42 -0200

    Commit - Evento click botão tabuleiro

--- src/main/java/br/com/brunoluz/app/botoes/EventoClickBotaoNivel.java
+++ src/main/java/br/com/brunoluz/app/botoes/EventoClickBotaoNivel.java
@@ -8,15 +8,26 @@ import javax.swing.JButton;
 public class EventoClickBotaoNivel implements ActionListener {
 	
 	static int quantidadeClique = 0;
-	private JButton botaoClicadoAnterior;
-	private JButton botaoClicadoAtual;
+	static JButton botaoClicadoAtual;
+	static JButton botaoClicadoAnterior;
+	
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		
 		quantidadeClique = (quantidadeClique < 2) ? quantidadeClique + 1 : 1;
 
-		mostarClique();
+		if (quantidadeClique == 1) 
+			botaoClicadoAtual = ((JButton) event.getSource());
+			
+		if (quantidadeClique == 2) {
+			botaoClicadoAnterior = botaoClicadoAtual;
+			botaoClicadoAtual = ((JButton) event.getSource());
+		}
+		
+		System.out.println("Atual " + nomeIcone(botaoClicadoAtual));
+		System.out.println("Anterior " + nomeIcone(botaoClicadoAnterior));
+		
 	}
 	
 	
@@ -24,12 +35,7 @@ public class EventoClickBotaoNivel implements ActionListener {
 	 * mostarClique
 	 */
 	private void mostarClique() {
-		
 		System.out.println("Atual : " + quantidadeClique);
-		
-		/*System.out.println("Anterior : " + nomeIcone(botaoClicadoAnterior));
-		System.out.println("Atual : " + nomeIcone(botaoClicadoAtual));*/
-		
 	}
 	
 	
@@ -39,6 +45,10 @@ public class EventoClickBotaoNivel implements ActionListener {
 	 * @return
 	 */
 	private String nomeIcone(JButton botao) {
+		
+		if (botao == null)
+			return null;
+		
 		String[] split = botao.getIcon().toString().split("/");
 		return split[split.length - 1];
 	}
