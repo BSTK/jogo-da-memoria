5415f86c3e992da4c6e139c12be920f4a87ef9ce
BSTK <bsilva.se@gmail.com>
2017-01-08 00:58:44 -0200

    Commit - Encontrando os pares

--- src/main/java/br/com/brunoluz/app/botoes/ClickIcone.java
+++ src/main/java/br/com/brunoluz/app/botoes/ClickIcone.java
@@ -4,7 +4,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import br.com.brunoluz.util.Icone;
 
 public abstract class ClickIcone extends MouseAdapter {
 	
@@ -21,6 +24,9 @@ public abstract class ClickIcone extends MouseAdapter {
 	private final int ICONE_NAO_ENCONTRADO = 1;
 	
 	public static int quantidadeCliques = 0;
+	
+	private static JPanel quadradoClique1;
+	private static JPanel quadradoClique2;
 	
 	
 	/**
@@ -39,10 +45,38 @@ public abstract class ClickIcone extends MouseAdapter {
 		
 		iconeTabuleiro.setVisible(!visivel);
 		iconeNaoEncontrado.setVisible(visivel);
-			
-		if (quantidadeCliques == 1)
-			quantidadeCliques = 0;
 		
+		if (quantidadeCliques == 1)
+			quadradoClique1 = quadradoTabuleiro;
+		
+		if (quantidadeCliques == 2) {
+			quadradoClique2 = quadradoTabuleiro;
+			
+			if (quadradoClique1 != null && quadradoClique2 != null && !quadradoClique1.equals(quadradoClique2)) {
+				
+				if (Icone.iguais(label(quadradoClique1), label(quadradoClique2))) {
+					JOptionPane.showMessageDialog(null, "Achou o par !!!");
+				}
+				else {
+					iconeTabuleiro.setVisible(visivel);
+					iconeNaoEncontrado.setVisible(!visivel);
+				}
+			}
+			
+			quantidadeCliques = 0;
+		}
+			
+	}
+	
+	
+	
+	/**
+	 * labelTabuleiro
+	 * @param panel
+	 * @return
+	 */
+	private JLabel label(JPanel panel) {
+		return (JLabel) panel.getComponent(ICONE_TABULEIRO);
 	}
 	
 }
--- src/main/java/br/com/brunoluz/util/Icone.java
+++ src/main/java/br/com/brunoluz/util/Icone.java
@@ -1,9 +1,10 @@
 package br.com.brunoluz.util;
 
-import static br.com.brunoluz.util.Constantes.PATH_ICONES;
 import static br.com.brunoluz.util.Constantes.ICONE_ESCONDIDO;
+import static br.com.brunoluz.util.Constantes.PATH_ICONES;
 
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 
 public class Icone {
@@ -24,6 +25,34 @@ public class Icone {
 	 */
 	public static ImageIcon comImagem(String icone) {
 		return new ImageIcon(Icone.class.getResource(String.format("%s%s", PATH_ICONES, icone)));
+	}
+	
+	
+	
+	/**
+	 * iguais
+	 * @param botao1
+	 * @param botao2
+	 * @return
+	 */
+	public static boolean iguais(JLabel botao1, JLabel botao2) {
+		return nomeIcone(botao1).equals(nomeIcone(botao2));
+	}
+	
+	
+	
+	/**
+	 * nomeIcone
+	 * @param botao
+	 * @return
+	 */
+	public static String nomeIcone(JLabel botao) {
+		
+		if (botao == null)
+			return null;
+		
+		String[] split = botao.getIcon().toString().split("/");
+		return split[split.length - 1];
 	}
 	
 }
